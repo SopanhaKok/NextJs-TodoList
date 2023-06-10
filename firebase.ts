45479a9e02a9999f525d8fa7e925a1169153560d
SopanhaKok <sopanhakok@gmail.com>
2023-06-10 20:14:27 +0700

    chore: update firebase config

--- firebase.ts
+++ firebase.ts
@@ -1,16 +1,12 @@
-import { initializeApp, cert } from 'firebase-admin/app'
 import admin from 'firebase-admin'
-import { getFirestore } from 'firebase-admin/firestore'
 
 import serviceAccount from './todo-list-firebase-adminsdk.json'
 
 if (!admin.apps.length) {
   console.log('initilizating')
-  initializeApp({
-    credential: cert(serviceAccount as any),
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount as any),
   })
 }
 
-const db = getFirestore()
-
-export default db
+export default admin.firestore()
