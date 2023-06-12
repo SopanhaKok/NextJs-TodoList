a851758a72ae1bb197689556e529d6ed8507aa3e
SopanhaKok <sopanhakok@gmail.com>
2023-06-12 14:02:41 +0700

    feat: local feature

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,6 @@
 import { useEffect, useState } from 'react'
 import WarningModal from './components/WarningModal'
 import { v4 as uuidv4 } from 'uuid'
-import axios from 'axios'
-import { onSnapshot, QuerySnapshot, DocumentData } from 'firebase/firestore'
-import { todosCollectionRef } from '@/app/utils/firestore.collection'
-import { BeatLoader } from 'react-spinners'
 
 import AddTodo from './components/AddTodo'
 
@@ -21,35 +17,19 @@ export default function Home() {
   const [currentTodoId, setCurrentTodoId] = useState<string>('')
   const [search, setSearch] = useState('')
   const [searchedTodos, setSearchedTodos] = useState<Todo[]>([])
-  const [loading, setLoading] = useState(false)
-
-  useEffect(() => {
-    setLoading(true)
-    const unsubcribe = onSnapshot(
-      todosCollectionRef,
-      (snapshot: QuerySnapshot<DocumentData>) => {
-        const updatedTodos: Todo[] = snapshot.docs
-          .map((doc) => ({
-            id: doc.id,
-            todo: doc.data().todo,
-            isCompleted: doc.data().isCompleted,
-            createdAt: doc.data().createdAt.toDate(),
-          }))
-          .sort((a, b) => b.createdAt - a.createdAt)
-
-        setTodos(updatedTodos)
-        setLoading(false)
-      }
-    )
-
-    return () => {
-      unsubcribe()
-    }
-  }, [])
 
   const onTodoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setTodo(e.target.value)
   }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem('todos') || '[]')
+    setTodos(todos)
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   const addTodo = async (e: React.FormEvent) => {
     e.preventDefault()
@@ -65,10 +45,7 @@ export default function Home() {
       createdAt: new Date(),
     }
 
-    const { status } = await axios.post('/api/todo', newTodo)
-    if (status === 200) {
-      setTodos([newTodo, ...todos])
-    }
+    setTodos([newTodo, ...todos])
     setTodo('')
   }
 
@@ -79,12 +56,8 @@ export default function Home() {
       }
       return todo
     })
-    const todo = todos.find((todo) => todo.id === id)
 
-    const { status } = await axios.post(`/api/todo/${id}`, { todo })
-    if (status === 200) {
-      setTodos(newTodos)
-    }
+    setTodos(newTodos)
   }
 
   const toggleEditMode = (id: string) => {
@@ -116,28 +89,18 @@ export default function Home() {
       return todo
     })
 
-    const todo = todos.find((todo) => todo.id === id)
-
-    const { status } = await axios.put(`/api/todo/${id}`, {
-      todo: newTodo,
-      isCompleted: todo?.isCompleted,
-    })
-
-    if (status === 200) {
-      setTodos(newTodos)
-      setTodo('')
-      setIsEdit(false)
-    }
+    setTodos(newTodos)
+    setTodo('')
+    setIsEdit(false)
   }
 
   const deleteTodo = async (id: string) => {
     const newTodos = todos.filter((todo) => todo.id !== id)
-    await axios.delete(`/api/todo/${id}`)
     setTodos(newTodos)
   }
 
-  const clearTodos = async () => {
-    await axios.post('/api/todo/clearTodo')
+  const clearTodos = () => {
+    setTodos([])
   }
 
   const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -172,9 +135,7 @@ export default function Home() {
               onChange={handleSearch}
             />
           </div>
-          {loading ? (
-            <BeatLoader className='mt-4' color='#2563EB' />
-          ) : search.length > 0 && searchedTodos.length > 0 ? (
+          {search.length > 0 && searchedTodos.length > 0 ? (
             <TodoList
               todos={searchedTodos}
               toggleEditMode={toggleEditMode}
